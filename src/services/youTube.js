9e26373a608e3fbbb9d9b6dbdd2aa5c4e631292b
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-09-10 15:15:11 +0300

    start youtube search

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,14 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function(cb){
+  	buildApiRequest('GET',
+                '/youtube/v3/search',
+                {'maxResults': '5',
+                 'part': 'snippet',
+                 'q': 'surfing',
+                 'type': '',
+                 'embeddable' : true,
+                 'key' : window.YOUTUBE_API_KEY
+             });
+
+     //cb(results)
 });
